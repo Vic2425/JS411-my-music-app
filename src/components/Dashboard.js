1f964f4d8055d47574eef4ae8b097d81d3c390c2
Vic <vicjs612@gmail.com>
2023-03-23 20:30:55 -0500

    add notifications for dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,38 @@
 import { React, useState } from "react";
 import "../App.css";
+import { Slider, Stack, Typography, Switch } from "@mui/material";
 import {
-  Box,
-  Slider,
-  Stack,
-  Typography,
-  Switch
+  Card,
+  CardContent,
+  CardActions,
+  Select,
+  InputLabel,
+  MenuItem,
+  FormControl,
 } from "@mui/material";
-import { Card, CardContent, CardActionArea, CardActions } from "@mui/material";
 
 export default function Dashboard() {
-  const [checked, setChecked] = useState(true);
-  const [value, setValue] = useState(0);
+  const [online, setOnline] = useState(true);
+  const [volume, setVolume] = useState(0);
+  const [quality, setQuality] = useState("");
 
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
+  const onlineChange = (event) => {
+    setOnline(event.target.checked);
   };
 
-  const volumeChange = (event, newValue) => {
-    setValue(newValue);
+  const volumeChange = (e) => {
+    setVolume(e.target.value);
+  };
+
+  const qualityChange = (e) => {
+    setQuality(e.target.value);
   };
 
   return (
     <div>
-      <Typography varian="h1" sx={{ mt: 2, ml: 3, fontSize: '24px'}}>Welcome user!</Typography>
+      <Typography variant="h4" align="center" sx={{ mt: 5, color: "#3a23eb" }}>
+        Welcome user!
+      </Typography>
       <Stack
         direction={{ xs: "column", sm: "row" }}
         spacing={{ xs: 1, sm: 2, md: 4 }}
@@ -31,37 +40,32 @@ export default function Dashboard() {
         justifyContent="center"
         sx={{ mt: 5 }}
       >
-        <Card sx={{ maxWidth: 300, minWidth: 200, height: 300 }}>
-          
-            <CardContent sx={{ m: 2 }}>
-              <Typography gutterBottom variant="h6" component="div">
-                Online Mode
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Is this application connected to the internet?
-              </Typography>
-            </CardContent>
+        <Card sx={{ maxWidth: 300, minWidth: 200, height: 250 }}>
+          <CardContent sx={{ m: 2 }}>
+            <Typography gutterBottom variant="h6" component="div">
+              Online Mode
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Is this application connected to the internet?
+            </Typography>
+          </CardContent>
           <CardActions sx={{ ml: 1, mt: 5 }}>
-            <Switch
-              checked={checked}
-              onChange={handleChange}
-              inputProps={{ "aria-label": "controlled" }}
-            />
+            <Switch checked={online} onChange={onlineChange} color="success" />
           </CardActions>
         </Card>
-        <Card sx={{ maxWidth: 300, minWidth: 200, height: 300 }}>
-            <CardContent sx={{ m: 2 }}>
-              <Typography gutterBottom variant="h6" component="div">
-                Master Volumn
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Overries all other sound settings in this application
-              </Typography>
-            </CardContent>
+        <Card sx={{ maxWidth: 300, minWidth: 200, height: 250 }}>
+          <CardContent sx={{ m: 2 }}>
+            <Typography gutterBottom variant="h6" component="div">
+              Master Volumn
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Overries all other sound settings in this application
+            </Typography>
+          </CardContent>
           <CardActions sx={{ width: 250, ml: 2, mt: 6 }}>
             <Slider
               aria-label="Volume"
-              defaultValue={20}
+              value={volume}
               onChange={volumeChange}
               step={10}
               marks
@@ -70,24 +74,44 @@ export default function Dashboard() {
             />
           </CardActions>
         </Card>
-        <Card sx={{ maxWidth: 300, minWidth: 200, height: 300 }}>
-            <CardContent sx={{ m: 2 }}>
-              <Typography gutterBottom variant="h6" component="div">
-                Online Mode
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Is this application connected to the internet?
-              </Typography>
-            </CardContent>
-          <CardActions sx={{ ml: 2, mt: 5 }}>
-            <Switch
-              checked={checked}
-              onChange={handleChange}
-              inputProps={{ "aria-label": "controlled" }}
-            />
+        <Card sx={{ maxWidth: 300, minWidth: 200, height: 250 }}>
+          <CardContent sx={{ m: 2 }}>
+            <Typography gutterBottom variant="h6" component="div">
+              Sound Quality
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Manually control the music quality in event of poor connection
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ ml: 2, mt: 6 }}>
+            <FormControl variant="standard" sx={{ width: 250 }}>
+              <InputLabel>Quality</InputLabel>
+              <Select value={quality} onChange={qualityChange}>
+                <MenuItem value={1}>Low</MenuItem>
+                <MenuItem value={2}>Normal</MenuItem>
+                <MenuItem value={3}>High</MenuItem>
+              </Select>
+            </FormControl>
           </CardActions>
         </Card>
       </Stack>
+      <div className="noticeBox">
+        <Typography variant="body1" sx={{ mt: 5, color: 'orange'}}>
+          {online === false
+            ? "Your application is offline. You will not be able to share or stream music to other devices."
+            : ""}
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 5, color: 'orange'}} >
+          {volume >= 80
+            ? "Listening to music at a high volume could cause long-term hearing loss."
+            : ""}
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 5, color: 'orange'}}>
+          {quality === 1
+            ? "Music quality is degraded. Increase quality if your connection allows it."
+            : ""}
+        </Typography>
+      </div>
     </div>
   );
 }
